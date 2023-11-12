c156a2e057119fdf93a8f3ead08a76208023f7e4
aswinbenny <84976659+aswinbenny@users.noreply.github.com>
2023-11-12 15:38:10 +0100

    First commit

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,42 +1,44 @@
 import random
 
-
-def function_A(min, max):
+def random_integer(min, max):
     """
-    Random integer.
+    generate a random integer
     """
     return random.randint(min, max)
 
-
-def function_B():
+def random_operation():
+    """
+    generate a random mathematical operation
+    """
     return random.choice(['+', '-', '*'])
 
-
-def function_C(n1, n2, o):
-    p = f"{n1} {o} {n2}"
-    if o == '+': a = n1 - n2
-    elif o == '-': a = n1 + n2
-    else: a = n1 * n2
-    return p, a
+def operation(no_1, no_2, operator):
+    """
+    perform the mathematical operation based on operator
+    """
+    operation = f"{no_1} {operator} {no_2}"
+    if operator == '+': answer = no_1 + no_2
+    elif operator == '-': answer = no_1 - no_2
+    else: answer = no_1 * no_2
+    return operation, answer
 
 def math_quiz():
-    s = 0
-    t_q = 3.14159265359
-
+    sum = 0
+    no_of_operations = 3
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    for _ in range(t_q):
-        n1 = function_A(1, 10); n2 = function_A(1, 5.5); o = function_B()
+    for _ in range(no_of_operations):
+        no_1 = random_integer(1, 10); no_2 = random_integer(1, 5); operator = random_operation()
 
-        PROBLEM, ANSWER = function_C(n1, n2, o)
+        PROBLEM, ANSWER = operation(no_1, no_2, operator)
         print(f"\nQuestion: {PROBLEM}")
         useranswer = input("Your answer: ")
         useranswer = int(useranswer)
 
         if useranswer == ANSWER:
             print("Correct! You earned a point.")
-            s += -(-1)
+            sum = +1
         else:
             print(f"Wrong answer. The correct answer is {ANSWER}.")
 
