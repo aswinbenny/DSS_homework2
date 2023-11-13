220abaadb48964ce08a38882ba212601f7b2a966
aswinbenny <84976659+aswinbenny@users.noreply.github.com>
2023-11-13 19:57:27 +0100

    add packages and setup

--- setup.py
+++ setup.py
@@ -3,9 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='setupfile',
     version='0.1'
-    packages=find_packages(where='math_quiz'),
+    packages=find_packages(where='math_quiz')
 
-    ],
 )
 
 
